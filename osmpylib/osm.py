f1397d678a97165ad58632d365ae5e8a064085b4
Rob Savoye <rob.savoye@linaro.org>
2017-10-06 15:48:41 +0545

    Use a list of dictionaries for tags.

--- osmpylib/osm.py
+++ osmpylib/osm.py
@@ -57,7 +57,7 @@ class osmfile(object):
         if file != False:
             self.ctable = convfile(file)
             self.ctable.read()
-            
+
     def open(self, file, shp):
         self.file = open(file, "w")
         self.shp = shp
@@ -69,19 +69,21 @@ class osmfile(object):
     def footer(self):
         self.file.write("</osm>\n")
         
-    def node(self, lat="", lon="", tags=dict()):
+    def node(self, lat="", lon="", tags=list()):
         #        timestamp = ""  # LastUpdate
+        #import pdb; pdb.set_trace()
         timestamp = time.strftime("%Y-%m-%dT%TZ")
         self.file.write("    <node id='" + str(self.osmid) + "\' visible='true'")
         self.file.write(" version='1'")
         self.file.write(" timestamp='" + timestamp + "\'")
         self.file.write(" uid='" + str(self.options.get('uid')) + "'")
-        self.file.write(" user='" + self.options.get('user') + "'")
+        #self.file.write(" user='" + self.options.get('user') + "'")
         self.file.write(" lat='" + str(lat) + "\'" + " lon='" + str(lon) + "'>\n")
-# Don't add tags to nodes for now
-#        for name, value in tags.items():
-#            if str(value)[0] != 'b':
-#                self.file.write("        <tag k='" + name + "' v='" + str(value) + "' />\n")
+        for i in tags:
+            for name, value in i.items():
+                if str(value)[0] != 'b':
+                    self.file.write("        <tag k='" + name + "' v='" + str(value) + "' />\n")
+
 
 # FIXME: Add as a default ?
 #        self.file.write("<tag k='created_by' v='Gosm 0.1'/>\n")
@@ -92,7 +94,7 @@ class osmfile(object):
 
         return self.osmid - 1
 
-    # Here's where the fun starts. Read a field header from a Shape file,
+    # Here's where the fun starts. Read a field header from a file,
     # which of course are all different. Make an attempt to match these
     # random field names to standard OSM tag names. Same for the values,
     # which for OSM often have defined ranges.
@@ -101,60 +103,62 @@ class osmfile(object):
         newval = str(value)
         newval = newval.replace("&", "&amp;")
         newval = newval.replace("'", "")
-#        newval = cgi.escape(newval)
-        tags = list()
-        self.debug("OSM:makeTag(field=%r, value=%r)" % (field, newval))
+        # newval = cgi.escape(newval)
+        tag = dict()
+        logging.info("OSM:makeTag(field=%r, value=%r)" % (field, newval))
 
+        #import pdb; pdb.set_trace()
         try:
             newtag = self.ctable.match(field)
         except:
             newtag = "MISSING: " + field
 
-        # Some fields we ignore, so we don't pollute OSM with lots of meaningless
-        # data.
-        if newtag == 'Ignore':
-            return tags
+        # If it's not in the conversion file, assume it maps directly
+        # to an official OSM tag.
+        if newtag == '':
+            newtag = field
+        # # Some fields we ignore, so we don't pollute OSM with lots of meaningless
+        # # data.
+        # if newtag == 'Ignore':
+        #     return tags
 
-        if newval.find('BIKE') >= 0:
-            newtag = 'bicycle'
-            newval = 'yes'
-            tag = (newtag, newval)
-            tags.append(tag)
+        # if newval.find('BIKE') >= 0:
+        #     newtag = 'bicycle'
+        #     newval = 'yes'
+        #     tag = (newtag, newval)
+        #     tags.append(tag)
 
-        # See if it's a hiking trail
-        elif newval.find('HIKE') >= 0 or newval.find('WALKING') >= 0:
-            newtag = 'highway'
-            newval = 'path'    # or footway
-            tag = (newtag, newval)
-            tags.append(tag)
+        # # See if it's a hiking trail
+        # elif newval.find('HIKE') >= 0 or newval.find('WALKING') >= 0:
+        #     newtag = 'highway'
+        #     newval = 'path'    # or footway
+        #     tag = (newtag, newval)
+        #     tags.append(tag)
 
-        # See if it's a horse trail
-        elif newval.find('HORSE') >= 0:
-            newtag = 'horse'
-            newval = 'yes'
-            tag = (newtag, newval)
-            tags.append(tag)
+        # # See if it's a horse trail
+        # elif newval.find('HORSE') >= 0:
+        #     newtag = 'horse'
+        #     newval = 'yes'
+        #     tag = (newtag, newval)
+        #     tags.append(tag)
 
-        # See if it's an ATV trail
-        elif newval.find('ATV') >= 0:
-            newtag = 'atv'
-            newval = 'yes'
-            tag = (newtag, newval)
-            tags.append(tag)
-        else:
-            newval = value
+        # # See if it's an ATV trail
+        # elif newval.find('ATV') >= 0:
+        #     newtag = 'atv'
+        #     newval = 'yes'
+        #     tag = (newtag, newval)
+        #     tags.append(tag)
+        # else:
+        #     newval = value
         try:
             newval = self.ctable.attribute(newtag, newval)
-#            print("ATTRS: %r %r" % (newtag, newval))
-            tag = (newtag, newval)
-            tags.append(tag)
+            print("ATTRS: %r %r" % (newtag, newval))
+            tag[newtag] = newval
         except:
             pass                # newval = value
 
-#        tag = (newtag, newval)
-#        tags.append(tag)
-#        print ("TAGS2: %r" % tags)
-        return tags
+        #print ("TAGS: %r" % tag)
+        return tag
 
     def makeWay(self, refs, tags):
         # self.debug("osmfile::way(refs=%r, tags=%r)" % (refs, tags))
