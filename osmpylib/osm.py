7453309350234ef917c3d37f45a647b775c33786
Rob Savoye <rob.savoye@linaro.org>
2017-11-15 11:50:15 -0700

    Process the tags in node creation step

--- osmpylib/osm.py
+++ osmpylib/osm.py
@@ -20,13 +20,21 @@ import os.path
 import time
 import logging
 from datafile import convfile
+from config import config
 
 
 class osmfile(object):
     """OSM File output"""
     def __init__(self, options, form=""):
         self.options = options
-        self.outfile = options.get('outdir') + '/' + form + ".osm"
+        #self.file = False
+        # Read the config file to get our OSM credentials, if we have any
+        self.config = config(self.options)
+        self.version = 3
+        self.visible = 'true'
+        self.osmid = -30470
+
+        self.outfile = self.options.get('outdir') + '/' + form + ".osm"
         # Open the OSM output file
         if self.outfile == False:
             self.outfile = "/tmp/foobar.osm"
@@ -39,36 +47,26 @@ class osmfile(object):
         except:
             logging.error("Couldn't open %s for writing!" % self.outfile)
 
-#         # Read the config file to get our OSM credentials, if we have any
-#         file = "/home/rob/.gosmrc"
-#         try:
-#             gosmfile = open(file, 'r')
-#         except:
-#             print("""WARNING: Couldn't open %s for writing! not using OSM credentials
-#             """ % file)
-#             return
-
-        self.version = 3
-        self.visible = 'true'
-        self.osmid = -30470
-
         # Read the conversion data
         file = self.options.get('convfile')
         if file != False:
             self.ctable = convfile(file)
             self.ctable.read()
 
-    def open(self, file, shp):
-        self.file = open(file, "w")
-        self.shp = shp
-        
+    def isclosed(self):
+        return self.file.closed
+
     def header(self):
-        self.file.write('<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n')
-        self.file.write('<osm version="0.6" generator="gosm 0.1" timestamp="2017-03-13T21:43:02Z">\n')
+        if self.file != False:
+            self.file.write('<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n')
+            self.file.write('<osm version="0.6" generator="gosm 0.1" timestamp="2017-03-13T21:43:02Z">\n')
 
     def footer(self):
+        #print("FIXME: %r" % self.file)
         self.file.write("</osm>\n")
-        
+        if self.file != False:
+            self.file.close()
+
     def node(self, lat="", lon="", tags=list()):
         #        timestamp = ""  # LastUpdate
         #import pdb; pdb.set_trace()
@@ -76,13 +74,15 @@ class osmfile(object):
         self.file.write("    <node id='" + str(self.osmid) + "\' visible='true'")
         self.file.write(" version='1'")
         self.file.write(" timestamp='" + timestamp + "\'")
-        self.file.write(" uid='" + str(self.options.get('uid')) + "'")
-        #self.file.write(" user='" + self.options.get('user') + "'")
+        self.file.write(" uid='" + str(self.config.get('uid')) + "'")
+        self.file.write(" user='" + self.config.get('user') + "'")
         self.file.write(" lat='" + str(lat) + "\'" + " lon='" + str(lon) + "'>\n")
         for i in tags:
             for name, value in i.items():
                 if str(value)[0] != 'b':
-                    self.file.write("        <tag k='" + name + "' v='" + str(value) + "' />\n")
+                    tag = self.makeTag(name, value)
+                    for newname, newvalue in tag.items():
+                        self.file.write("        <tag k='" + newname + "' v='" + str(newvalue) + "' />\n")
 
 
 # FIXME: Add as a default ?
