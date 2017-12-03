6a1eafbc3654b66e4943900f9c454c790dd38b98
Rob Savoye <rob.savoye@linaro.org>
2017-12-02 18:46:35 -0700

    Needed for pip

---
+++ osmpylib/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Gosm',
+    version='0.1',
+    packages=[],
+    url='http://www.github.com/rsavoye/gosm.git',
+    license='GPLv3',
+    author='Rob Savoye',
+    author_email='rob@senecass.com',
+    description='Various utilities for OSM data files.',
+)
